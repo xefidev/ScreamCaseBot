33f846f21c5febb2695ca8d13f5a848b9f08f9b9
Xefi <amogus003negr@gmail.com>
2026-06-12 14:31:14 +0300

    perf(app): force animations on (remove simple mode toggle effect)

--- src/App.jsx
+++ src/App.jsx
@@ -406,7 +406,7 @@ export default function App() {
   };
 
   return (
-    <MotionConfig reducedMotion={lowPerf ? "always" : "never"}>
+    <MotionConfig reducedMotion="never">
     <div className="h-screen w-full overflow-hidden flex justify-center items-center bg-[#1a1b1e] text-white font-rounded select-none">
       <div className="relative z-10 flex flex-col h-screen w-full max-w-md bg-[#1a1b1e] overflow-hidden">
         <div className="shrink-0">
